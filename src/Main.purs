2b5436e684e93ce7f7076e95f2490e68b3ac22a3
Chris Myers <chris.myers@rea-group.com>
2017-05-19 08:58:10 +1000

    Trying to get forever working

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,26 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
--- import Control.Monad.Aff (delay)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Now (NOW, now)
+import Run (Run, liftBase, runBase)
+import Run.Streaming.Prelude (forever)
 
-import Effect.Mraa (HWIO, Pin(..), dirOut, gpio, mraa, write)
 
-main :: forall e. Eff (hwio :: HWIO | e) Unit
+main :: forall e. Eff ( now :: NOW, console :: CONSOLE | e) Unit
 main = do
-  let m = mraa
-  ledPin <- gpio m $ Pin 44
-  dirOut m ledPin
-  write ledPin 1
+  -- let m = mraa
+  -- ledPin <- gpio m $ Pin 44
+  -- dirOut m ledPin
+  -- runBase $ forever $ liftBase (onOff ledPin)
+  runBase $ forever $ liftBase ( now )
+
+-- xxx :: forall a. Run (  a )
+
+
+-- onOff :: forall e. Gpio -> Eff (hwio :: HWIO, console :: CONSOLE| e) Unit
+-- onOff ledPin = do
+--   log "Turning on led"
+--   write ledPin 1
+--   log "Turning off led"
+--   write ledPin 0
