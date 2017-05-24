cc15caed4c50973dbfc14342015b8d5f606a55bd
Chris Myers <chris.myers@rea-group.com>
2017-05-24 16:35:21 +1000

    Works using run-streaming

--- src/Main.purs
+++ src/Main.purs
@@ -1,52 +1,50 @@
 module Main where
 
-import Control.Coroutine as Co
-import Control.Coroutine (runProcess, ($$))
-import Control.Monad (class Monad)
-import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Now (NOW, now)
-import Control.Monad.Rec.Class (forever)
-import Control.Monad.Trans.Class (lift)
 import Data.DateTime.Instant (Instant, unInstant)
-import Data.Int (floor)
-import Data.Maybe (Maybe(..))
-import Data.Show (class Show, show)
+import Data.Int (floor, toNumber)
 import Data.Time.Duration (Milliseconds(..))
-import Effect.Mraa (Gpio, HWIO, Pin(..), dirOut, gpio, mraa, write)
+import Effect.FakeMraa (Gpio, HWIO, Pin(..), dirOut, gpio, mraa, write)
 import Math ((%))
-import Prelude (Unit, bind, discard, pure, void, ($), (/))
+import Prelude (Unit, bind, discard, pure, void, (#), ($), (+), (<<<), (>=))
+import Run (Run, liftBase, runBase)
+import Run.Streaming.Prelude (foldM, produce)
 
+type PinState = { pin :: Gpio, lastUpdate :: Number, lastValue :: Int}
+type World = { pinState :: PinState }
 
-main :: forall e. Eff (now :: NOW, hwio :: HWIO,  exception :: EXCEPTION, console :: CONSOLE | e) Unit
+main :: forall e. Eff (now :: NOW, hwio :: HWIO, exception :: EXCEPTION, console :: CONSOLE | e) Unit
 main = do
   let m = mraa
   ledPin <- gpio m $ Pin 44
   dirOut m ledPin
-  void $ launchAff $ runProcess (timeStream $$ (sink ledPin))
+  let timeStream = produce $ liftBase now
+  let world = { pinState : {pin : ledPin, lastUpdate : 0.0 , lastValue : 0}}
+  timeStream # (runIt $ world) # runBase # void
 
-showing :: forall a m. Show a => Monad m => Co.Transformer a String m Unit
-showing = forever (Co.transform show)
+runIt :: World -> Run (_) Unit -> Run (_) World
+runIt world = foldM (\w i -> liftBase $ process w i) (pure world) pure
 
-sink :: forall eff. Gpio -> Co.Consumer Instant (Aff (hwio :: HWIO, console :: CONSOLE | eff)) Unit
-sink led = forever do
-  s <- Co.await
-  lift (liftEff (onOff led s))
-  pure Nothing
-
-timeStream :: forall eff. Co.Producer Instant (Aff (now :: NOW | eff)) Unit
-timeStream = do
-  n <- lift (liftEff now)
-  Co.emit n
-  timeStream
-
-onOff :: forall e. Gpio -> Instant -> Eff (hwio :: HWIO, console :: CONSOLE| e) Unit
-onOff ledPin i = do
-  let val = floor $ (fromMilli $ unInstant i)/1000.0 % 2.0
-  logShow val
-  write ledPin val
+process :: forall eff.
+  World ->
+  Instant ->
+  Eff ( console :: CONSOLE, hwio :: HWIO | eff )  World
+process world instant = if i >= (world.pinState.lastUpdate + 1000.0)
+  then do
+    logShow i
+    let newValue = floor $ toNumber (world.pinState.lastValue + 1) % 2.0
+    write world.pinState.pin newValue
+    pure ({pinState: { pin: world.pinState.pin, lastUpdate: i, lastValue: newValue}})
+  else
+    pure world
   where
-  fromMilli (Milliseconds m) = m
+    i = instantToNumber instant
+
+instantToNumber :: Instant -> Number
+instantToNumber = fromMilli <<< unInstant
+
+fromMilli :: Milliseconds -> Number
+fromMilli (Milliseconds m) = m
